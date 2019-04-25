a3400911f11a69fa8b9708d839a0e4f6faff7876
witty-banner-1950 <asimsali95@gmail.com>
2019-04-25 03:27:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(num)
   if num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0 
-    return "buzz"
+    return "Buzz"
   elsif num % 3 == 0 && num % 5 == 0 
     return "fizzbuzz"
   else 
